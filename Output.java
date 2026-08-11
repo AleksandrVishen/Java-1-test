624eba569de4dbfea04afbea6f29362adbdb124f
AleksandrVishen <aleksandrkirezi@gmail.com>
2026-08-11 15:31:32 +0300

    tt

---
+++ Output.java
@@ -0,0 +1,8 @@
+public class Output {
+    public static void main(String[] args) {
+        int integer = 5;
+        double doub = 5.0;
+        System.out.println("Вишня" + " 22 года"); // Конкатенация строк
+        System.out.println((integer / 2) + " " + (doub / 2)); // Различие целого и дробного типа
+            }
+}
