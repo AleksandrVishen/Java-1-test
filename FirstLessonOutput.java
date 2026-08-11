81697855189ca51aaf4f01bed07f5d913938fb89
AleksandrVishen <aleksandrkirezi@gmail.com>
2026-08-11 15:39:49 +0300

    FirstLesson Difference between print and println

---
+++ FirstLessonOutput.java
@@ -0,0 +1,10 @@
+public class FirstLessonOutput {
+    public static void main(String[] args) {
+        String name = "Vishen";
+        System.out.println("Hi! My name is " + name +".");
+        System.out.print("I am ");
+        int age = 22;
+        System.out.print(age);
+        System.out.print(" years old.");
+    }
+}
