600ad73a8502cc23d65449658e45888834d6a714
AleksandrVishen <aleksandrkirezi@gmail.com>
2026-08-13 21:25:57 +0300

    Intellij

---
+++ KeyboardInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class KeyboardInput {
+    public static void main(String[] args) {
+        System.out.println("Введите имя: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        
+        System.out.println("Привет, " + name + "!");
+    }
+}
